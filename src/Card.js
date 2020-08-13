6245caa078b051c93c6cb6bd3e64888bd51cec78
Robert Shields <robert.shieldsiv@gmail.com>
2020-08-13 13:35:29 -0400

    added props for card

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
-const Card = () =>{
+//props
+const Card = ({name, email, id}) =>{
     return(
-        <div>
-           <img alt="robots" src="" />> 
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+           <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>Jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
-
     );
 }
 
