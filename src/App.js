8a5e191bcb9bcba0224d030a52792b4b5acf9349
Robert Shields <robert.shieldsiv@gmail.com>
2020-08-19 11:27:06 -0400

    added state to searchbox

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import { robots } from './robots'
 import SearchBox from './SearchBox'
 
 
-const App = () => {
-    return(
+class App extends React.Component {
+    constructor() {
+        super()
+            this.state = {
+                robots: robots,
+                searchfield: ''
+            }
+    }
+    onSearchChange(event){
+        console.log(event.target.value);
+    }
+
+    render(){
+        return(
         <div className='tc'>
             <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots= { robots }/>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots= { this.state.robots }/>
 
         </div>
       
     );
+    }
+
 }
 
 export default App;
