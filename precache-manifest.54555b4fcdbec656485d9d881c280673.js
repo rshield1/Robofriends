63224d152debd9b990c754eeb44ca5e709cdd759
Robert Shields <robert.shieldsiv@gmail.com>
2020-08-19 17:17:25 -0400

    Updates

---
+++ precache-manifest.54555b4fcdbec656485d9d881c280673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2835b9000d7df79ff456e312dfbfe46f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f8e04d9277d94f60b4e7",
+    "url": "/robofriends/static/css/main.765312ab.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e04d9277d94f60b4e7",
+    "url": "/robofriends/static/js/main.62a755fd.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
